e1d2dd57545190cf6df92e95050b6911dfdc65f5
Fahad Hossain <fahad.hossain@westwing.de>
2022-02-16 12:09:51 +0100

    feat: add experimental setup

---
+++ index.js
@@ -0,0 +1,5 @@
+import nestedLodash from "./nested-micro-thing/index.js"
+
+import lodash from 'lodash'
+
+console.log({ nestedLodashVersion: nestedLodash.VERSION, parentLodashVersion: lodash.VERSION })
